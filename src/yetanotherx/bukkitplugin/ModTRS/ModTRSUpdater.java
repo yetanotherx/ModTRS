20ac60572b6d632675d4f81ce8b8bbc5d8b4d37a
Yetanotherx <yetanotherx@gmail.com>
2011-04-05 23:57:12 -0400

    remove update code, it's corrupting the jar

--- src/yetanotherx/bukkitplugin/ModTRS/ModTRSUpdater.java
+++ src/yetanotherx/bukkitplugin/ModTRS/ModTRSUpdater.java
@@ -1,10 +1,10 @@
 package yetanotherx.bukkitplugin.ModTRS;
 
 import java.io.File;
-import java.io.FileOutputStream;
+/*import java.io.FileOutputStream;
 import java.net.URL;
 import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.ReadableByteChannel;*/
 
 public class ModTRSUpdater {
 
@@ -17,15 +17,22 @@ public class ModTRSUpdater {
 	File file = new File("plugins" + File.separator + "ModTRS", "sqlitejdbc-v056.jar");
 
 	if( !file.exists() ) {
+
+	    ModTRS.log.severe("Please manually download the SQLite JDBC from http://www.zentus.com/sqlitejdbc/ and place in the plugins/ModTRS/ folder.");
+	    parent.getServer().getPluginManager().disablePlugin(parent);
+	    return false;
+	    //This is broken, it corrupts the jar
+
+	    /*
 	    ModTRS.log.info("Downloading SQLite library from GitHub directory");
 
 	    try {
 		URL u = new URL("https://github.com/yetanotherx/ModTRS/raw/master/resources/sqlitejdbc-v056.jar");
-		
+
 		ReadableByteChannel rbc = Channels.newChannel(u.openStream());
 		FileOutputStream fos = new FileOutputStream(file.getAbsolutePath());
 		fos.getChannel().transferFrom(rbc, 0, 1 << 24);
-		
+
 		if( file.exists() ) {
 		    ModTRS.log.info("Successfully downloaded the SQLite JDBC. Please restart the Minecraft server.");
 		    parent.getServer().getPluginManager().disablePlugin(parent);
@@ -45,7 +52,10 @@ public class ModTRSUpdater {
 	else {
 	    ModTRS.log.debug("Found SQLite library. Will not download");
 	}
-	
+
+	return true;*/
+	}
+
 	return true;
 
     }
